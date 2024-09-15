f7824d01f70202a8721311ed4523280e9ec022d7
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-09-15 17:44:01 -0300

    feat: Adiciona migração inicial e atualiza configurações do projeto
    - Adicionada a primeira migração do Entity Framework Core, incluindo as tabelas para categorias e produtos.
    
    - Atualizado o arquivo CleanArchMvc.Infra.Data.csproj para incluir pacotes do Entity Framework Core.
    
    - Incluída configuração da connection string no appsettings.json.

---
+++ CleanArchMvc.Infra.Data/Migrations/20240915200516_ProdutoInit.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProdutoInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products (Name, Description, Price, Stock, Image, CategoryId) VALUES " +
+                   "('Martelo', 'Martelo de aço forjado', 29.99, 50, 'martelo.jpg', 1)," +
+                   "('Chave de Fenda', 'Chave de fenda com cabo ergonômico', 9.99, 100, 'chave_fenda.jpg', 1)");
+            
+            mb.Sql("INSERT INTO Products (Name, Description, Price, Stock, Image, CategoryId) VALUES " +
+                   "('Estetoscópio', 'Estetoscópio profissional', 199.99, 30, 'estetoscopio.jpg', 2)," +
+                   "('Termômetro Digital', 'Termômetro digital com leitura rápida', 24.99, 80, 'termometro.jpg', 2)");
+            
+            mb.Sql("INSERT INTO Products (Name, Description, Price, Stock, Image, CategoryId) VALUES " +
+                   "('Lápis', 'Lápis HB número 2', 0.99, 200, 'lapis.jpg', 3)," +
+                   "('Caderno', 'Caderno espiral 100 folhas', 7.99, 150, 'caderno.jpg', 3)");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products WHERE CategoryId IN (1, 2, 3)");
+        }
+    }
+}
