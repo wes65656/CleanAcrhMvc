2007b726f49e7c4864025e22759654c4ce12becc
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-09-19 12:51:44 -0300

    feat: implementar Interface serviço de categoria.

---
+++ CleanArchMvc.Aplication/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+using CleanArchMvc.Aplication.DTOs;
+
+namespace CleanArchMvc.Aplication.Interfaces;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDto>> GetCsetgories();
+    Task<CategoryDto> GetById(int? id);
+
+    Task Add(CategoryDto categoryDto);
+    Task Update(CategoryDto categoryDto);
+    Task Remove(int? id);
+}
