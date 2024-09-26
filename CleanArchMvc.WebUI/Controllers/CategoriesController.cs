f0cf1dc291bc011a86b0cd221ee31dbc8268a24b
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-09-26 11:56:50 -0300

    feat: adicionar métodos Create, Edit, Delete e Details no CategoriesController CRUD

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using CleanArchMvc.Aplication.DTOs;
 using CleanArchMvc.Aplication.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,5 +19,87 @@ public class CategoriesController : Controller
     {
         var categories = await _categoryService.GetCategories();
         return View(categories);
+    }
+
+    [HttpGet()]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryDto category)
+    {
+        if (ModelState.IsValid)
+        {
+            await _categoryService.Add(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(category);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id is null) return NotFound();
+        
+        var categoryDto = await _categoryService.GetById(id);
+
+        if (categoryDto is null) return NotFound();
+        return View(categoryDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryDto categoryDto)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _categoryService.Update(categoryDto);
+            }
+            catch (Exception problemException)
+            {
+                Console.WriteLine(problemException);
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(categoryDto);
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id is null) return NotFound();
+
+        var categoryDto = await _categoryService.GetById(id);
+
+        if (categoryDto is null) return NotFound();
+
+        return View(categoryDto);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> ConfirmedDelete(int id)
+    {
+        await _categoryService.Remove(id);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id is null) 
+            return NotFound();
+
+        var categoryDto = await _categoryService.GetById(id);
+
+        if (categoryDto is null) 
+            return NotFound();
+        
+        return View(categoryDto);
     }
 }
