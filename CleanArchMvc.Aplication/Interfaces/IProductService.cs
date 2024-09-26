d0737c71ec2da12ebd50d7db24bb3fe56d326e33
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-09-26 12:13:20 -0300

    refactor: Remover método GetProductCategory da interface IProductService

--- CleanArchMvc.Aplication/Interfaces/IProductService.cs
+++ CleanArchMvc.Aplication/Interfaces/IProductService.cs
@@ -6,8 +6,7 @@ public interface IProductService
 {
     Task<IEnumerable<ProductDto>> GetProducts();
     Task<ProductDto> GetById(int? id);
-    Task<ProductDto> GetProductCategory(int? id);
-    
+    // Task<ProductDto> GetProductCategory(int? id);
     Task Add (ProductDto productDto);
     Task Update (ProductDto productDto);
     Task Remove (int? id);
