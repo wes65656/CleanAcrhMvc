de2be9904de5104f40cd5a6e5e4fa4d836599dd6
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-09-28 16:50:56 -0300

    feat: Implementa funcionalidades de gerenciamento de produtos no ProductsController
    - Adiciona métodos para criar, editar, excluir e visualizar detalhes de produtos.
    - Injeta ICategoryService para gerenciar categorias de produtos.
    - Injeta IWebHostEnvironment para manipular arquivos de imagem.

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,18 +1,27 @@
+using System.Drawing;
+using System.IO;
 using System.Threading.Tasks;
+using CleanArchMvc.Aplication.DTOs;
 using CleanArchMvc.Aplication.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
+            _categoryService = categoryService;
             _productService = productService;
+            _environment = environment;
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
@@ -20,5 +29,82 @@ namespace CleanArchMvc.WebUI.Controllers
             return View(products);
         }
 
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryID = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Create(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto is null) return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null) return NotFound();
+            var productoDto = await _productService.GetById(id);
+
+            if (productoDto is null) return NotFound();
+
+            return View(productoDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        { 
+            await _productService.Remove(id);
+            return RedirectToAction("Index");
+        }
+        
+        [HttpGet()]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id is null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto is null) return NotFound();
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "Images", productDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExists = exists;
+
+            return View(productDto);
+        }
     }
 }
